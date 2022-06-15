8157a4911aa48e4416a7d4e97f1895fab8f5958e
Luis Martins <chateadorn@gmail.com>
2022-06-15 15:07:52 -0300

    menu things/2

--- sketch.js
+++ sketch.js
@@ -273,7 +273,7 @@ function draw() {
       
       textSize(25);
       fill(0, 0, 0);
-      text("Jogar novamente", 135, 295);
+      text("Jogar novamente", 105, 295);
       
       circle(mouseX, mouseY, 10);
         
